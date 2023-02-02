dd84bc999de402c1915a2c121da3ef969465d654
Hyeri1ee <haerizian10@gmail.com>
2023-02-02 23:48:51 +0900

    baekjoon and chap06 5

---
+++ Baekjoon/baekjoon_problem/src/stage/deck1/Num3003.java
@@ -0,0 +1,26 @@
+package stage.deck1;
+
+import java.util.Scanner;
+
+public class Num3003 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int king = 1-sc.nextInt();
+		int queen = 1-sc.nextInt();
+		int look = 2-sc.nextInt();
+		int bishop = 2-sc.nextInt();
+		int night = 2-sc.nextInt();
+		int phone = 8-sc.nextInt();
+		System.out.println(king);
+		System.out.println(queen);
+		System.out.println(look);
+		System.out.println(bishop);
+		System.out.println(night);
+		System.out.println(phone);
+		
+		
+			
+	}
+
+}
