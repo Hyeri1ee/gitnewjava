8688baa694623dc8bf8f3fc04d118f5e6bfdfabb
Hyeri1ee <haerizian10@gmail.com>
2023-02-03 02:32:36 +0900

    message ?

---
+++ Baekjoon/baekjoon_problem/src/stage/deck2/Num2525.java
@@ -0,0 +1,15 @@
+package stage.deck2;
+
+import java.util.Scanner;
+
+public class Num2525 {
+
+	public static void main(String[] args) {
+		/*
+		 * 백준 번호 : 2525
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
